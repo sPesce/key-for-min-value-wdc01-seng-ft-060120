e6ba6b983ead87218a1197de17efbcd3f1c2c225
stephen pesce <stevepesce879@gmail.com>
2020-05-20 22:04:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@ def key_for_min_value(name_hash)
     return nil 
   end
   min_val  = (2**(0.size * 8 -2) -1) #max fixnum val | source: http://nizhu.github.io/ruby-intro-site/blog/2013/07/28/numbers/
-  p min_val 
   min_key = :init 
   
+  #for each value in name_hash : set new min_val, min_key if smaller than current
   name_hash.each{|(key,value)|
     if value < min_val
       min_val = value
@@ -14,5 +14,4 @@ def key_for_min_value(name_hash)
     end
   }
   min_key 
-  
 end
