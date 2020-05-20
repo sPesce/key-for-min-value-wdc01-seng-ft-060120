ee3b7e90b0f9d25edcdf3b5e66eed98a5e5c4e98
stephen pesce <stevepesce879@gmail.com>
2020-05-20 22:04:16 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash == {}
+    return nil 
+  end
+  min_val  = (2**(0.size * 8 -2) -1) #max fixnum val | source: http://nizhu.github.io/ruby-intro-site/blog/2013/07/28/numbers/
+  min_key = :init 
+  
+  #for each value in name_hash : set new min_val, min_key if smaller than current
+  name_hash.each{|(key,value)|
+    if value < min_val
+      min_val = value
+      min_key = key
+    end
+  }
+  min_key 
 end
