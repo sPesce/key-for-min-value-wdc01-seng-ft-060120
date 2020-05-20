fbeb2622ff67cb8855e627dfb3bbe1dfdaf429db
stephen pesce <stevepesce879@gmail.com>
2020-05-20 21:50:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,5 @@
 
 end
 def key_for_min_value(name_hash)
-  min_val  = (2**(0.size * 8 -2) -1) #max fixnum val | source: http://nizhu.github.io/ruby-intro-site/blog/2013/07/28/numbers/
+  #min_val  = (2**(0.size * 8 -2) -1) #max fixnum val | source: http://nizhu.github.io/ruby-intro-site/blog/2013/07/28/numbers/
 end
